c790523bc682bfc032f6692409505cd36eab44eb
Muzammil8989 <muzammilkhan89223@gmail.com>
2025-04-05 10:43:57 +0500

    Update ALL

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/styles/main.css";
 import AppRoutes from "./routes";
+import logo from "./assets/images/logo.png";
 
 function App() {
-  return <AppRoutes />;
+  return (
+    <div className="app-container">
+      <img src={logo} alt="Logo" className="logo" />
+      <AppRoutes />
+    </div>
+  );
 }
 
 export default App;
